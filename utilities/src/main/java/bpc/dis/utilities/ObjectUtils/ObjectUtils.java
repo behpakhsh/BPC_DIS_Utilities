715ddbf060d7a80a8f6112b99fcecd2fb1354c63
Behpakhsh It Department <behpakhsh.it@gmail.com>
2019-12-01 09:11:29 +0330

    add ObjectUtils class

---
+++ utilities/src/main/java/bpc/dis/utilities/ObjectUtils/ObjectUtils.java
@@ -0,0 +1,42 @@
+package bpc.dis.utilities.ObjectUtils;
+
+import java.util.List;
+import java.util.Map;
+
+public class ObjectUtils {
+
+    public static boolean isNull(Object object) {
+        return object == null;
+    }
+
+    public static boolean isNullOrEmpty(List objects) {
+        return objects == null || objects.size() <= 0;
+    }
+
+    public static boolean isNullOrEmpty(Map objects) {
+        return objects == null || objects.size() <= 0;
+    }
+
+    public static boolean isNotNullOrEmpty(Map objects) {
+        if (objects == null) {
+            return false;
+        }
+        return objects.size() > 0;
+    }
+
+    public static boolean isNotNullOrEmpty(List objects) {
+        if (objects == null) {
+            return false;
+        }
+        return objects.size() > 0;
+    }
+
+    public static boolean isNotEmpty(List objects) {
+        return objects.size() > 0;
+    }
+
+    public static boolean isNotEmpty(Map objects) {
+        return objects.size() > 0;
+    }
+
+}
