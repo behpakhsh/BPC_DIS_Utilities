fa4a0791ca7fba8bef91c5203e178cc1400d98a4
Behpakhsh It Department <behpakhsh.it@gmail.com>
2019-12-02 08:13:08 +0330

    MeasureHelper

--- utilities/src/main/java/bpc/dis/utilities/MeasureHelper/MeasureHelper.java
+++ utilities/src/main/java/bpc/dis/utilities/MeasureHelper/MeasureHelper.java
@@ -5,7 +5,7 @@ import android.util.TypedValue;
 
 public class MeasureHelper {
 
-    public static float pixelToDip(Context context, int pixel) {
+    public static float pixelToDip(Context context, float pixel) {
         return TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, pixel, context.getResources().getDisplayMetrics());
     }
 
