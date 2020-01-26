1d34fd4d239a4b625cf04a59c4936b69ff8e6c5f
Behpakhsh It Department <behpakhsh.it@gmail.com>
2020-01-26 10:12:08 +0330

    add param to Device Helper

--- utilities/src/main/java/bpc/dis/utilities/DeviceHelper/DeviceInfoHelper.java
+++ utilities/src/main/java/bpc/dis/utilities/DeviceHelper/DeviceInfoHelper.java
@@ -2,6 +2,7 @@ package bpc.dis.utilities.DeviceHelper;
 
 import android.annotation.SuppressLint;
 import android.app.Activity;
+import android.content.Context;
 import android.os.Build;
 import android.provider.Settings;
 import android.util.DisplayMetrics;
@@ -9,6 +10,7 @@ import android.util.DisplayMetrics;
 @SuppressLint("HardwareIds")
 public class DeviceInfoHelper {
 
+    @Deprecated
     public static DeviceInfo getDeviceInfo(Activity activity) {
         DisplayMetrics displayMetrics = activity.getResources().getDisplayMetrics();
         DeviceInfo deviceInfo = new DeviceInfo();
@@ -21,8 +23,20 @@ public class DeviceInfoHelper {
         return deviceInfo;
     }
 
-    public static String getDeviceUniqueId(Activity activity) {
-        return Settings.Secure.getString(activity.getContentResolver(), Settings.Secure.ANDROID_ID) + "_" + Build.SERIAL;
+    public static DeviceInfo getDeviceInfo(Context context) {
+        DisplayMetrics displayMetrics = context.getResources().getDisplayMetrics();
+        DeviceInfo deviceInfo = new DeviceInfo();
+        deviceInfo.setDeviceName(Build.BRAND + "_" + Build.MODEL);
+        deviceInfo.setOsVersion(Build.VERSION.SDK_INT);
+        deviceInfo.setResolution((int) (displayMetrics.density * 160f));
+        deviceInfo.setScreenSize(displayMetrics.widthPixels + "*" + displayMetrics.heightPixels);
+        deviceInfo.setUniqueId(getDeviceUniqueId(context));
+        deviceInfo.setBuildSerial(Build.SERIAL);
+        return deviceInfo;
+    }
+
+    private static String getDeviceUniqueId(Context context) {
+        return Settings.Secure.getString(context.getContentResolver(), Settings.Secure.ANDROID_ID) + "_" + Build.SERIAL;
     }
 
 }
