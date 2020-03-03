cd06ad1ce5b6b2254cc4f7e1534013db5f8e9b4a
Behpakhsh It Department <behpakhsh.it@gmail.com>
2020-03-03 10:31:35 +0330

    Add -> getString() method to Class ResourceManagerListener

--- utilities/src/main/java/bpc/dis/utilities/ResourceManager/ResourceManagerListener.java
+++ utilities/src/main/java/bpc/dis/utilities/ResourceManager/ResourceManagerListener.java
@@ -5,12 +5,15 @@ import android.graphics.drawable.Drawable;
 import androidx.annotation.ColorRes;
 import androidx.annotation.DimenRes;
 import androidx.annotation.DrawableRes;
+import androidx.annotation.StringRes;
 
 public interface ResourceManagerListener {
 
     float getDimension(@DimenRes int resId);
 
     int getColor(@ColorRes int resId);
+
+    String getString(@StringRes int resId);
 
     Drawable getDrawable(@DrawableRes int resId);
 
