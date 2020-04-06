470ec42ff53c6d369b86b53cb1df8ed6420db341
Behpakhsh It Department <behpakhsh.it@gmail.com>
2020-04-06 21:46:44 +0330

    Create -> Class ApplicationHelper

---
+++ utilities/src/main/java/bpc/dis/utilities/ApplicationHelper.java
@@ -0,0 +1,25 @@
+package bpc.dis.utilities;
+
+import android.app.ActivityManager;
+import android.content.Context;
+
+import java.util.List;
+
+public class ApplicationHelper {
+
+    public static boolean isAppllicationRunning(Context context, String packageName) {
+        ActivityManager activityManager = (ActivityManager) context.getSystemService(Context.ACTIVITY_SERVICE);
+        if (activityManager != null) {
+            List<ActivityManager.RunningAppProcessInfo> runningAppProcesses = activityManager.getRunningAppProcesses();
+            if (runningAppProcesses != null && !runningAppProcesses.isEmpty()) {
+                for (ActivityManager.RunningAppProcessInfo runningAppProcess : runningAppProcesses) {
+                    if (runningAppProcess.processName.equals(packageName)) {
+                        return true;
+                    }
+                }
+            }
+        }
+        return false;
+    }
+
+}
